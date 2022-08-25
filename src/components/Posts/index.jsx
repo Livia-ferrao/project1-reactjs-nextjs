d19faa190787169227d6fc4636d2f150361e4278
Lívia <livia.ferrao16@hotmail.com>
2022-08-25 17:45:17 -0300

    Configuração do eslint, prettier, editorconfig e prop-types

--- src/components/Posts/index.jsx
+++ src/components/Posts/index.jsx
@@ -1,10 +1,32 @@
-import { PostCard } from '../PostCard'
-import './styles.css'
+import { PostCard } from "../PostCard";
+import "./styles.css";
+import P from "prop-types";
 
-export const Posts = ({ posts }) => (
-    <div className="posts">
-    {posts.map(post => (
-     <PostCard title={post.title} id={post.id} body={post.body} cover={post.cover} />
+export const Posts = ({ posts = [] }) => (
+  <div className="posts">
+    {posts.map((post) => (
+      <PostCard
+        key={post.id}
+        title={post.title}
+        id={post.id}
+        body={post.body}
+        cover={post.cover}
+      />
     ))}
   </div>
-)
+);
+
+Posts.defaultProps = {
+  posts: [],
+};
+
+Posts.propTypes = {
+  posts: P.arrayOf(
+    P.shape({
+      title: P.string.isRequired,
+      cover: P.string.isRequired,
+      body: P.string.isRequired,
+      id: P.number.isRequired,
+    })
+  ),
+};
---
+++ src/templates/Home/Home.test.jsx
@@ -0,0 +1,5 @@
+describe("<home />", () => {
+  it("teste one", () => {
+    expect(1).toBe(1);
+  });
+});
